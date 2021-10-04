2e23ea216104a2d1c7b4dba85883e2e7e115e680
Orhan Albayati <70097003+Orhan92@users.noreply.github.com>
2021-10-04 13:59:56 +0200

    Settings for Telemtry

--- Pages/Create.cshtml.cs
+++ Pages/Create.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CosmosWebApp;
 using CosmosWebApp.Models;
+using Microsoft.ApplicationInsights;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -12,19 +13,26 @@ namespace CosmosWebApp.Pages
     public class CreateModel : PageModel
     {
         private readonly ICosmosDbService _cosmosDbService;
+        private TelemetryClient _aiClient;
         public CreateModel(ICosmosDbService cosmosDbService)
         {
             _cosmosDbService = cosmosDbService;
         }
+        public CreateModel(TelemetryClient aiClient)
+        {
+            _aiClient = aiClient;
+        }
 
         [BindProperty]
         public Song Music { get; set; }
-        public async Task <IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync()
         {
-                Music.Id = Guid.NewGuid().ToString();
-                Music.Created = DateTime.Now;
-                await _cosmosDbService.AddItemAsync(Music);           
-                return RedirectToPage("/Index");         
+            Music.Id = Guid.NewGuid().ToString();
+            Music.Created = DateTime.Now;
+            await _cosmosDbService.AddItemAsync(Music);
+            _aiClient.TrackEvent("AddedSong");
+            return RedirectToPage("/Index");
+
         }
     }
 }
