93469d1c8eedba765a43289cfcd1787d6232eb2e
Orhan Albayati <70097003+Orhan92@users.noreply.github.com>
2021-10-06 13:08:38 +0200

    removed nanespaces

--- Program.cs
+++ Program.cs
@@ -1,14 +1,9 @@
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Events;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CosmosWebApp
 {
